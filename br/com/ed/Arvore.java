e17998da75158e3ce1deaa4d5c1d3ef90e5619da
lucassena08 <Lucassena0810@gmail.com>
2023-05-29 20:38:37 -0300

    implementa inserirAEsquerda, retirarAEsquerda e
    getTamanho

--- br/com/ed/Arvore.java
+++ br/com/ed/Arvore.java
@@ -2,12 +2,13 @@ package br.com.ed;
 
 public class Arvore {
     public No raiz;
+    public long tamanho = 0;
 
-    public Arvore () {
+    public Arvore() {
         raiz = null;
     }
 
-    public boolean inserir (int info) {
+    public boolean inserir(int info) {
         No atual = raiz;
 
         No no = new No(info);
@@ -26,12 +27,14 @@ public class Arvore {
                 atual = atual.esquerda;
                 if (atual == null) {
                     no.pai.esquerda = no;
+                    tamanho++;
                     return true;
                 }
             } else {
                 atual = atual.direita;
                 if (atual == null) {
                     no.pai.direita = no;
+                    tamanho++;
                     return true;
                 }
             }
@@ -39,7 +42,7 @@ public class Arvore {
         return false;
     }
 
-    public boolean inserirADireita (int info) {
+    public boolean inserirADireita(int info) {
         No atual = raiz;
 
         No no = new No(info);
@@ -59,6 +62,7 @@ public class Arvore {
             atual = atual.direita;
             if (atual == null) {
                 no.pai.direita = no;
+                tamanho++;
                 return true;
             }
         }
@@ -66,7 +70,7 @@ public class Arvore {
         return false;
     }
 
-    public boolean inserirAEsquerda (int info) {
+    public boolean inserirAEsquerda(int info) {
         No atual = raiz;
 
         No no = new No(info);
@@ -80,7 +84,80 @@ public class Arvore {
             no.pai = atual;
 
             if (info == atual.info) {
+                return false;
+            }
 
+            atual = atual.esquerda;
+            if (atual == null) {
+                no.pai.esquerda = no;
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
+    public boolean retirarADireita() {
+        No atual = raiz;
+
+        if (atual.direita == null) {
+            return false;
+        }
+
+        while (atual != null) {
+            atual = atual.direita;
+
+            if (atual.direita == null) {
+                atual.pai.direita = null;
+                tamanho--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean retirarAEsquerda() {
+        No atual = raiz;
+
+        if (atual.esquerda == null) {
+            return false;
+        }
+
+        while (atual != null) {
+            atual = atual.esquerda;
+
+            if (atual.esquerda == null) {
+                atual.pai.esquerda = null;
+                tamanho--;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public long getTamanho() {
+        return tamanho;
+    }
+
+    public boolean ehUmaFolha(int info) {
+        No atual = raiz;
+
+        if (atual.esquerda == null && atual.direita == null) {
+            return true;
+        }
+
+        while (atual != null) {
+            if (info == atual.info) {
+                if (atual.esquerda == null && atual.direita == null) {
+                    return true;
+                }
+            } else if (info < atual.info) {
+                atual = atual.esquerda;
+            } else {
+                atual = atual.direita;
             }
         }
 
